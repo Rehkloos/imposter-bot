d5502cb7e4ddf8b1d41efa3f1bea3757bb569245
Andre Saddler <dresadd09691@gmail.com>
2020-09-12 03:16:09 -0400

    add basic 'mute' command
    - Toggle mute everyone in the active VC

--- commands/help.js
+++ commands/help.js
@@ -27,6 +27,7 @@ class HelpCommand extends Command {
       .addField('!ping first', 'Pings only the first person in line in the queue.')
       .addField('!format', 'shows recommended lobbies settings')
       .addField('!create', 'create Among Us category, voice channel and queue voice channels')
+      .addField('!mute or !m', 'Toggle mute everyone in the active VC')
       .addField('!overlay [channel name]', 'Generates a Discord Overlay URL for the given channel (defaults to the channel you\'re currently in).')
       .addFields({
         name: '.',
---
+++ commands/mute.js
@@ -0,0 +1,47 @@
+const {
+  Command
+} = require('discord-akairo');
+const L = require('../logger');
+
+
+var alreadyMuted = false;
+
+class MuteCommand extends Command {
+  constructor() {
+    super('mute', {
+      aliases: ['mute', 'm'],
+      channel: 'guild'
+    });
+  }
+
+  async exec(msg) {
+    // deafen and mutme evryone
+    var _game = "";
+    try {
+      _game = msg.guild.channels.cache.find(channel => channel.name.includes("Among"));
+    } catch (err) {}
+    if (msg.member.voice.channel) { // check if people are in channel
+      if (msg.member.voice.channel.id === _game.id) { // check for channel titled "among"
+        var channel = msg.guild.channels.cache.get(msg.member.voice.channel.id);
+        alreadyMuted = !alreadyMuted;
+        for (const [memberID, member] of channel.members) {
+          if (alreadyMuted) {
+            member.voice.setMute(true);
+            //member.voice.setDeaf(true);
+            msg.channel.send(`Sssh, the round begins, silence.`);
+          } else {
+
+            member.voice.setMute(false);
+            //member.voice.setDeaf(false);
+            msg.channel.send(`The round of discussion initiated.`);
+          }
+        }
+      } else {
+        msg.channel.send("You must be in the game channel to use this command!");
+      }
+    } else {
+      msg.channel.send("You must be in the game channel to use this command!");
+    }
+  }
+}
+module.exports = MuteCommand;

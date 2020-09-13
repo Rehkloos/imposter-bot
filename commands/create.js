bc1e1003e8eb1693fcf055618d5dec34fd99e802
Andre Saddler <dresadd09691@gmail.com>
2020-09-13 04:13:22 -0400

    Create: enhanced create command
    - create Permission flags
    - create channels/category creation checks
    - create multiple text channels at once
    - ETC

--- commands/create.js
+++ commands/create.js
@@ -13,26 +13,72 @@ class CreateCommand extends Command {
 
   async exec(msg) {
     msg.delete();
-    const channelName = 'Among-gen';
+    const gen = 'Among-gen';
     const categoryName = 'Among Us';
+    const codes = 'codes';
+    const queue = 'queue';
+    //const parentCat = msg.guild.channels.cache.find(channel => channel.name.includes(categoryName));
 
-    msg.guild.channels.create(categoryName, {
-      type: 'category',
-      permissionOverwrites: [{
+    const everyoneTEXT = [
+      // everyone
+      {
         id: msg.guild.id,
-        allow: ['VIEW_CHANNEL'],
-      }]
-    }).then(createdChannel => {
-      var id = createdChannel.id;
-      msg.guild.channels.create(channelName, {
-        type: 'voice',
-        parent: id,
-        permissionOverwrites: [{
-          id: msg.guild.id,
-          allow: ['VIEW_CHANNEL'],
-        }]
+        allow: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'READ_MESSAGE_HISTORY'],
+      },
+    ];
+
+
+    const everyoneVC = [
+      // everyone
+      {
+        id: msg.guild.id,
+        allow: ['VIEW_CHANNEL', 'SPEAK', 'CONNECT'],
+      },
+    ];
+
+    const maxCAP = 10;
+
+    if ((!msg.guild.channels.cache.find(c => c.name === categoryName))) {
+      msg.guild.channels.create(categoryName, {
+        type: 'category',
+        permissionOverwrites: everyoneTEXT
+      }).then(createdChannel => {
+        var id = createdChannel.id;
+        msg.guild.channels.create(gen, {
+          type: 'voice',
+          parent: id,
+          userLimit: maxCAP,
+          permissionOverwrites: everyoneVC
+        });
       });
-    });
+    } else if ((msg.guild.channels.cache.find(c => c.name === categoryName))) {
+      msg.channel.send(`Among Us Channel exists`).then(msg => {
+          msg.delete({
+            timeout: 10000
+          });
+        })
+        .catch(console.error);
+    }
+
+    if ((!msg.guild.channels.cache.find(c => c.name === gen)) || (!msg.guild.channels.cache.find(c => c.name === codes)) || (!msg.guild.channels.cache.find(c => c.name === queue))) {
+      for (var i = 1; i < 3; i++) { // CREATE 2 text rooms
+        msg.guild.channels.create(codes + String(i), {
+          type: 'text',
+          permissionOverwrites: everyoneTEXT
+        });
+      }
+      msg.guild.channels.create(queue, {
+        type: 'voice',
+        permissionOverwrites: everyoneVC
+      });
+    } else if ((msg.guild.channels.cache.find(c => c.name === gen)) || (msg.guild.channels.cache.find(c => c.name === codes)) || (msg.guild.channels.cache.find(c => c.name === queue))) {
+      msg.channel.send(`General, Codes, & Queue Channels exists`).then(msg => {
+          msg.delete({
+            timeout: 10000
+          });
+        })
+        .catch(console.error);
+    }
   }
 }
 

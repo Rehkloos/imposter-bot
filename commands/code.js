5181886858b75e76b8ea310c55c0ed09e3e7ba50
Andre Saddler <dresadd09691@gmail.com>
2020-09-14 01:20:22 -0400

    code: only send code embed to codes channel

--- commands/code.js
+++ commands/code.js
@@ -3,6 +3,10 @@ const {
 } = require('discord-akairo');
 const L = require('../logger');
 
+const {
+  MessageEmbed
+} = require('discord.js');
+
 const blacklistedWords = new Set(['NICE', 'OKAY', 'STFU', 'WHOA', 'GUYS', 'LMAO', 'ROFL', 'FUCK', 'BRUH', 'SHIT', 'WHAT', 'LULW', 'KEKW', 'LOLW', 'DUDE', 'HAHA', 'AHAH', 'LMOA', 'JOIN', 'COME']);
 
 class CodeCommand extends Command {
@@ -16,7 +20,10 @@ class CodeCommand extends Command {
 
   async exec(msg) {
     if (!blacklistedWords.has(msg.content)) {
-      await msg.react('👀');
+
+      var gamechannelID = "";
+      gamechannelID = msg.guild.channels.cache.find(channel => channel.name.includes("AmongUs | "));
+
       let oldCode = await this.client.settings.get(msg.guild.id, 'code', '');
       if (oldCode === msg.content) return;
       await this.client.settings.set(msg.guild.id, 'code', msg.content);
@@ -34,13 +41,45 @@ class CodeCommand extends Command {
           }
         }
       }
-      maxChannel.edit({
-        name: `Voice | ${msg.content}`
-      });
-      msg.guild.me.edit({
-        nick: `${msg.content} | ${this.client.user.username}`
-      });
-      L.log(`Updated code to ${msg.content}`);
+
+      if (msg.member.voice.channel && gamechannelID) {
+        try {
+          var codesID = msg.guild.channels.cache.find(channel => channel.name === "codes").id;
+          const mychannel = (msg.guild.channels.cache.find(c => c.name === "codes"));
+          if (mychannel) mychannel.send(new MessageEmbed()
+            .setColor("#3A92EF")
+            .setTitle(`${msg.content}`)
+            .setDescription(`The code is ${msg.content}.\n\nCheck the voice channel name too!\n*sometimes the voice channel name wont change due to being rate limited*`) // make this look better
+            .setTimestamp()
+            .setFooter(`Requested by ${msg.author.username}`, msg.author.avatarURL)
+          ).then(msg => {
+            msg.delete({
+              timeout: 1000 * 60 * 60 * 5
+            });
+          });
+          if (!msg.channel.id === codesID) {
+            // check if the command wasnt sent in the codes channel
+            msg.channel.send(`Sent in <#${codesID}>`).then(msg => {
+              msg.delete({
+                timeout: 5000
+              });
+            });
+          }
+
+        } catch (err) {
+          msg.channel.send(mychannel);
+        }
+
+        maxChannel.edit({
+          name: `AmongUs | ${msg.content}`
+        });
+        msg.guild.me.edit({
+          nick: `${msg.content} | ${this.client.user.username}`
+        });
+        L.log(`Updated code to ${msg.content}`);
+      } else {
+        msg.channel.send("**Error:** Please join a voice channel.");
+      }
     }
   }
 }
